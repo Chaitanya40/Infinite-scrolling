7706427cff4c4e4941a429ab2a039367289aee7a
Chaitanya40 <chaitanyaallu1605@gmail.com>
2016-09-22 15:14:47 +0530

    integrate inspinia theme with the application

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(0..100).each do |row|
+  prod = Product.new
+  prod.description = (0...8).map { (65 + rand(26)).chr }.join
+  prod.sku = (0...2).map { (65 + rand(26)).chr }.join
+  prod.quantity = rand(10)
+  prod.save
+end  
